af14ad60c46226586249efe92dfa95b995fa90c5
Sumali Junuthula <sumalijunuthula@Sumalis-Air.ssid1>
2025-06-16 18:12:58 -0400

    Add base to vite.config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/sumali-junuthula.github.io/',
   plugins: [react(), tailwindcss()],
 })
